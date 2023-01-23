974f60a29f56d445c4983a37ccd56deb01a79eac
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2023-01-24 01:20:27 +0530

    this is shiva added file of java descriotion

---
+++ code/java1.java
@@ -0,0 +1,35 @@
+import java.nio.file.attribute.UserPrincipal;
+
+import javax.security.auth.callback.LanguageCallback;
+
+class Main{
+    public static void main(String[] args){
+        // since quality is going UserPrincipal
+        // redesigen this code
+        // why java is famous 
+        // when you talk about java whihc are in top 5 LanguageCallback
+        // java is used for app development web development enterprise level development
+        // in java 
+        // we have jvm
+        // kotlin ,scala , groovy you indirectly used java
+        // in interview question you will be asked 
+        // java 
+        // coproate trainer and software developer
+        // work for 12 years
+
+        // java is readable Language
+        // maintable 
+        // multi-
+        // wora 
+        // exception handling
+        // java is platform independent
+        // multithreaded Language
+        // colection api
+
+        // history 1995 -sun microsystem
+        // oracle brought
+        // java update in every 6 month
+
+
+    }
+}
---
+++ code/java2.java
@@ -0,0 +1,69 @@
+ 
+
+public class java2 {
+   public static void main(String []args){
+    //notepad, notepad++, sublime, visual studio code, eclipse, intellij
+    //ide is a software which is used to write code and compile and run the code
+    //vs code is a free software which is used to write code and compile and run the code
+    //eclipse is a free software which is used to write code and compile and run the code
+
+    // editor example 
+    // notepad, notepad++, sublime, visual studio code, eclipse, intellij
+    //difference between ide and editor
+    //editor is a software which is used to write code
+    //ide is a software which is used to write code and compile and run the code 
+// visual studio vs vs code 
+//visual studio  is a editor or ide  
+
+    // YOU NEED COmpiler 
+    // download java jdk from oracle website
+    // or open source jdk -- coretto 
+    //java 19 is launch 
+    //lts is java 17 
+    //windows, mac, linux
+
+    // in window you set the path 
+
+    // vs code -- popular ide 
+    // just download and install
+    //just go next next next 
+
+    // setup for vscode 
+    // next next 
+
+    // cmd --just type java --version
+    // javac --version  for compiler
+    JDK stands for Java Development Kit, which is a software development environment used for developing Java applications and applets. It includes the Java Runtime Environment (JRE), as well as various development tools such as the Java compiler and debuggers.
+
+    JRE stands for Java Runtime Environment, which is the environment in which Java applications and applets run. It includes the Java Virtual Machine (JVM), as well as the Java class libraries and other resources that are necessary for running Java programs.
+
+// what is difference between jre and jvm 
+//jre is a collection of tools and component  and jvm is a collection of tools and component 
+
+//what is jdk 
+//jdk is java development kit use to develop java application and provide the tools to compile and run the code 
+//what is jre
+//jre is java runtime environment use to run the java application 
+//what is jvm
+//jvm is java virtual machine use to run the java application 
+//    just open environment variable
+//you can choose either user or system variable
+// what is the difference between user and system variable
+// user variable is only for that user  and system variable is for all user
+//what is means by only for that user 
+//if you are using multiple user in your system then you can set the path for that user only
+//example you have 3 user in your system then you can set the path for that user only 
+
+//    path -- edit -- new -- paste the path of bin folder of jdk
+//c drive -- program files -- java -- jdk -- bin 
+//    c:\program files\java\jdk-17\bin 
+
+// for coretoo  
+//you need to download the coretoo from amazon website free of cost
+//you need not manually set the path of coretoo 
+//run java --version and javac --version if you get the version then you are good to go
+//else set the path of coretoo
+//c drive -- program files -- amazon corretto -- jdk17.0.1_12 -- bin
+//c:\program files\amazon corretto\jdk17.0.1_12\bin
+} 
+}
