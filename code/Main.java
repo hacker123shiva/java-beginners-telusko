f429695535fd00e49c8d7b4d06253af1c8adae2e
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2023-02-23 12:27:05 +0530

    this is shiva

--- code/Main.java
+++ code/Main.java
@@ -1,23 +1,13 @@
- //throws keyword is used to handle the checked exception
-   //checked exception is the exception that is checked at compile time 
-   //for example IOException, SQLException etc.
-   //throws keyword is used to declare an exception 
-   //throws keyword is always used with method signature
-
-
-   //without handling in try catch block
-   //we can duck the exception by using throws keyword 
-
-   class Main
-   {
-	   public static void main(String []args){
-System.out.println("shiva");
-A obj =()->System.out.println("shiva");
-obj.hashCode();
-	   }
+class Main{
+   public static void main(String []args){
+      int a=0;
+      try{
+         if(a==0)
+throw new ArithmeticException("a should not be zero");
+      }
+      catch(ArithmeticException e){
+         System.out.println("Exception caught: "+e);
+      }
+    
    }
-   
-
-   interface A{
-void show();
-   }
+}
