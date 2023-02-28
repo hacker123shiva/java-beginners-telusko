79826b0bea5811b7c1b07028f5604c5299fa621d
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2023-02-28 20:10:27 +0530

    this  is shiva

--- code/Main.java
+++ code/Main.java
@@ -1,13 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class Main{
-   public static void main(String []args){
-      int a=0;
-      try{
-         if(a==0)
-throw new ArithmeticException("a should not be zero");
+   public static void main(String[] args) throws IOException {
+           BufferedReader br = null;
+           try {
+               // create a new InputStreamReader to read from System.in
+               InputStreamReader isr = new InputStreamReader(System.in);
+               // create a new BufferedReader to read from the InputStreamReader
+               br = new BufferedReader(isr);
+   
+               System.out.println("Enter your name:");
+               // read a line of text from the BufferedReader
+               String name = br.readLine();
+   
+               System.out.println("Hello, " + name + "!");
+           }  
+           finally{  
+            if(br!=null)
+             br.close();     
       }
-      catch(ArithmeticException e){
-         System.out.println("Exception caught: "+e);
-      }
-    
+ }
    }
-}
+   
+ 
