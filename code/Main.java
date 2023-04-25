221fdca71436d964425bc6af70ec19f09c485255
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2023-04-25 08:40:21 +0530

    add java file

--- code/Main.java
+++ code/Main.java
@@ -1,27 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-class Main{
-   public static void main(String[] args) throws IOException {
-           BufferedReader br = null;
-           try {
-               // create a new InputStreamReader to read from System.in
-               InputStreamReader isr = new InputStreamReader(System.in);
-               // create a new BufferedReader to read from the InputStreamReader
-               br = new BufferedReader(isr);
-   
-               System.out.println("Enter your name:");
-               // read a line of text from the BufferedReader
-               String name = br.readLine();
-   
-               System.out.println("Hello, " + name + "!");
-           }  
-           finally{  
-            if(br!=null)
-             br.close();     
-      }
+ class Main{
+  public static void main(String []arags){
+B obj=new B();
+obj.show();
+  }
  }
-   }
-   
- 
+
+ class A{
+private int a;
+protected int b;
+public int c;
+int d;
+ }
+
+ class B extends A{
+public void show(){
+// System.out.println(a); // error
+System.out.println(b); // ok
+System.out.println(c);
+System.out.println(d);
+ }
+}
